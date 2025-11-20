3a28ff374a7d73383f61c35a817026080ba97ec1
Masungulo-ark <Masungulomaluleka@gmail.com>
2025-11-20 14:24:51 +0200

    inital commit

--- User Interface/src/components/Navbars/AdminNavbar.js
+++ User Interface/src/components/Navbars/AdminNavbar.js
@@ -17,6 +17,7 @@
 */
 import { Link } from "react-router-dom";
 // reactstrap components
+
 import {
   DropdownMenu,
   DropdownItem,
@@ -45,18 +46,8 @@ const AdminNavbar = (props) => {
           >
             {props.brandText}
           </Link>
-          <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
-            <FormGroup className="mb-0">
-              <InputGroup className="input-group-alternative">
-                <InputGroupAddon addonType="prepend">
-                  <InputGroupText>
-                    <i className="fas fa-search" />
-                  </InputGroupText>
-                </InputGroupAddon>
-                <Input placeholder="Search" type="text" />
-              </InputGroup>
-            </FormGroup>
-          </Form>
+      
+       
           <Nav className="align-items-center d-none d-md-flex" navbar>
             <UncontrolledDropdown nav>
               <DropdownToggle className="pr-0" nav>
